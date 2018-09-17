7f5d7d501d9970d1837bcf7773e357268bf341eb
bigweld <bigweld86@gmail.com>
2018-09-16 20:04:44 -0700

    Footer implemented

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,12 +1,37 @@
-<!-- Footer -->
-<section class="footer-container">
-  <h2>Footer</h2>
-</section>
-<!-- </div> -->
+@include('layouts.modal')
 
+<footer class="page-footer font-small blue pt-4">
+  <div class="container footer-content">
+    <div class="container-fluid text-center text-md-left">
+      <div class="row">
+        <div class="col-md-6 mt-md-0 mt-3">
+          <h5 class="text-uppercase">ShipWire</h5>
+          <p>An INGRAM micro brand</p>
+        </div>
+        <hr class="clearfix w-100 d-md-none pb-3">
+        <div class="col-md-3 mb-md-0 mb-3">
+            <ul class="list-unstyled">
+              <li><a href="#!">About Us</a></li>
+              <li><a href="#!">Partnerships</a></li>
+            </ul>
+        </div>
+        <div class="col-md-3 mb-md-0 mb-3">
+          <ul class="list-unstyled">
+            <li><a href="#!">Careers</a></li>
+            <li><a href="#!">Contact Us</a></li>
+          </ul>
+        </div>
+    </div>
+  </div>
+
+  <div class="footer-copyright text-center py-3">© 2018 Copyright:
+    <a href="mailto:jsebastianbo@gmail.com"> Sebastian Bonilla</a>
+  </div>
+  </div>
+</footer>
 
 <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+  <script src="{{ asset('js/app.js') }}"></script>
 
   
 </body>
