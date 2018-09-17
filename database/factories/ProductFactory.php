8500a4e30fe067c638e4bf304dae15dbc1e41d35
bigweld <bigweld86@gmail.com>
2018-09-16 19:58:37 -0700

    Product factory updated

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -6,7 +6,7 @@ $factory->define(App\Product::class, function (Faker $faker) {
     return [
         'product_name' => $faker->sentence($nbWords = 4, $variableNbWords = true),
         'product_description' => $faker->paragraph($nbSentences = 6, $variableNbSentences = true),
-        'product_price' => $faker->numberBetween($min=500, $max=2000),
+        'product_price' => $faker->numberBetween($min=1, $max=10000),
         'product_width' => $faker->numberBetween($min=1, $max=300),
         'product_length' => $faker->numberBetween($min=1, $max=300),
         'product_height' => $faker->numberBetween($min=1, $max=300),
