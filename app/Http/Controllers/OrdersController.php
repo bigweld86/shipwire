da83e0f7f78d464195df7d806c859ba54a8b0672
bigweld <bigweld86@gmail.com>
2018-09-18 19:37:37 -0700

    index method was updated to check for existing orders. initCheckout and completeCheckout methods were created

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -12,6 +12,9 @@ class OrdersController extends Controller
     public function index() : Object
     {
         $order = Order::where(['order_status' => 1])->first();
+
+        if (is_null($order)) return view('orders.noorders');
+
         $data = $order->getAssociatedDataArray();   
         return view('orders.index', ['order' => $order, 'orderDetails' => $data]);
     }
@@ -40,6 +43,34 @@ class OrdersController extends Controller
     public function initCheckout() : Object
     {
         return view('orders.checkout');
+    }
+
+    public function completeCheckout(Request $request) : Object
+    {
+        // validate data
+        $request->validate([  
+            'order_customer_first_name' => 'required',
+            'order_customer_last_name' => 'required',
+            'order_address' => 'required',
+            'order_city' => 'required',
+            'order_state' => 'required',
+            'order_zip' => 'required'
+        ]);
+
+        $order = Order::where(['order_status' => 1])->first();
+        if (is_null($order)) return view('orders.noorders');
+
+        $order->update([
+            'order_status' => 2,
+            'order_customer_first_name' => $request->order_customer_first_name,
+            'order_customer_last_name' => $request->order_customer_last_name,
+            'order_address' => $request->order_address,
+            'order_city' => $request->order_city,
+            'order_state' => $request->order_state,
+            'order_zip' => $request->order_zip
+        ]);
+        
+        return redirect('products')->with('success', 'Your order is now being processed by our great customer associates');
     }
 
 }
